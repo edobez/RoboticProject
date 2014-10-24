09e4f040b4bf6a9c10ac50b68166c098a3fe79a4
Edoardo Bezzeccheri <coolman3@gmail.com>
2014-10-24 20:04:24 +0200

    Gestione finestre migliorata
    - Fix gestione finestre

--- Scripts/graphResults.m
+++ Scripts/graphResults.m
@@ -1,20 +1,21 @@
-% close all
-
 %% Acquisizione dati da output modello
 pose.sp = transl(out.get('Tsp'));
 pose.plan = transl(out.get('Tplan'));
 
-q.sp = out.get('q');
-q.dot = out.get('qdot');
+q.sprad = out.get('q');
+q.sp = out.get('q')*180/pi;
+q.dot = out.get('qdot')*180/pi;
 q.dot = q.dot(1:length(q.sp),:);
-q.man = robot.maniplty(q.sp);
+q.man = robot.maniplty(q.sprad);
 
 t = out.get('tout');
 t = t(1:length(q.sp));
 
 %% Grafico proiezioni traiettorie
 scrsz = get(groot,'ScreenSize');
-f1 = figure('OuterPosition',[1 50 scrsz(3)/4 (scrsz(4) - 50)]);
+f2 = figure(2);
+set(f2,'OuterPosition',[1 50 scrsz(3)/4 (scrsz(4) - 50)]);
+clf(f2)
 
 % XY
 subplot(3,1,1)
@@ -53,21 +54,23 @@ axis([-1 1 -0.5 2]);
 hold off
 
 %% Grafico joint
-f2 = figure('OuterPosition',[scrsz(3)/4  50 scrsz(3)/4 (scrsz(4) - 50)]);
+f3 = figure(3);
+set(f3,'OuterPosition',[scrsz(3)/4  50 scrsz(3)/4 (scrsz(4) - 50)]);
+clf(f3)
 
 subplot(3,1,1)
 plot(t*ones(1,6),q.sp(:,:))
 title('Joint positions (set-point)')
 xlabel('Time')
-ylabel('Rad')
-yaxis([-pi/2 pi/2])
+ylabel('deg')
+% yaxis([-pi/2 pi/2])
 legend('q1','q2','q3','q4','q5','q6')
 
 subplot(3,1,2)
 plot(t*ones(1,6),q.dot(:,:))
 title('Joint velocities (set-point)')
 xlabel('Time')
-ylabel('Rad/s')
+ylabel('deg/s')
 
 subplot(3,1,3)
 plot(t,q.man)
--- Scripts/initSys.m
+++ Scripts/initSys.m
@@ -1,4 +1,4 @@
-clear, clc, close all;
+clear, clc
 
 %% Script di inizializzazione
 s = tf('s');
@@ -18,11 +18,11 @@ tau_max = 0.9;
 
 Mimo = eye(6) * (1/(J*s + B));
 
-T1 = transl(0,0.2,0) * T0;
-T2 = transl(0,0,-0.3) * T1;
-T3 = transl(0,-0.2,0) * T2;
+T1 = transl(-0.6,0.5,0.8) * troty(-pi/2);
+T2 = transl(-0.6,-0.5,0.8)* troty(-pi/2);
+T3 = transl(-0.6,0,0.3)* troty(-pi/4);
 
 %% Simulazione
-
+figure(1)
 out = sim('System');
 graphResults;
