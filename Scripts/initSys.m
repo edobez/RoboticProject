1fbbd7a6c12df4aaeaafad324b20a5b8d3541327
Edoardo Bezzeccheri <coolman3@gmail.com>
2014-10-25 12:42:35 +0200

    Modello TOP
    - Aggiunta modello "top" con parte mia e di Ale per testare se SimMech funziona. A quanto pare con VariableStep va, ma non con il Fixed, cosa che ci darà problemi in futuro
    
    - Il modello in cui entrano gli input in coppia è stato messo in Old/ perchè non è più aggiornato
    
    - System è stato cambiato per fare delle prove

--- Scripts/initSys.m
+++ Scripts/initSys.m
@@ -21,6 +21,7 @@ Mimo = eye(6) * (1/(J*s + B));
 T1 = transl(-0.4,0.5,0.8) * troty(-pi/2);
 T2 = transl(-0.4,-0.5,0.8)* troty(-pi/2);
 T3 = transl(-0.4,0,0.3)* troty(-pi/4);
+T4 = transl(0,-0.8,0) * T0;
 
 %% Simulazione
 figure(1)
